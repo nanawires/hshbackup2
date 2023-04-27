c7787d6bef6791defea8b73bb731acfdd7a41196
nanawires <nanagyb@gmail.com>
2023-04-27 00:31:19 +0000

    updated files

---
+++ realloc.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ * _memset - memory is filled with a constant byte
+ * @s: memory area pointer
+ * @b: the byte to fill *s with
+ * @n: byte amount to be filled
+ * Return: (s) a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+		s[k] = b;
+	return (s);
+}
+
+/**
+ * ffree - frees string
+ * @pp: string
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates memory block
+ * @ptr: previous malloc block pointer
+ * @old_size: previous block byte size
+ * @new_size: new block byte size
+ *
+ * Return: old block pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *q;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	q = malloc(new_size);
+	if (!q)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		q[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (q);
+}
