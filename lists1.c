653c7f2bba4a5effa7826891db688a1b0c3ba266
nanawires <nanagyb@gmail.com>
2023-04-27 02:16:44 +0000

    updated files

---
+++ lists1.c
@@ -0,0 +1,122 @@
+#include "shell.h"
+
+/**
+ * list_len - determines linked list length
+ * @h: first node pointer
+ *
+ * Return: list size
+ */
+size_t list_len(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h)
+	{
+		h = h->next;
+		a++;
+	}
+	return (i);
+}
+
+/**
+ * list_to_strings - returns a string array
+ * @head: first node pointer
+ *
+ * Return: array of strings
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t a = list_len(head), b;
+	char **strs;
+	char *str;
+
+	if (!head || !a)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (a + 1));
+	if (!strs)
+		return (NULL);
+	for (a = 0; node; node = node->next, a++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (b = 0; b < a; b++)
+				free(strs[b]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[a] = str;
+	}
+	strs[a] = NULL;
+	return (strs);
+}
+
+
+/**
+ * print_list - prints elements of a list_t linked list
+ * @h: first node pointer
+ *
+ * Return: list size
+ */
+size_t print_list(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
+
+/**
+ * node_starts_with - returns prefix starting strings
+ * @node: list head pointer
+ * @prefix: matching string
+ * @c: next character after matching prefix
+ *
+ * Return: match node or null
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * get_node_index - gets the index of a node
+ * @head: list head pointer
+ * @node: pointer to the node
+ *
+ * Return: index of node or -1 otherwise
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t a = 0;
+
+	while (head)
+{
+	if (head == node)
+		return (a);
+	head = head->next;
+	a++;
+}
+return (-1);
+}
